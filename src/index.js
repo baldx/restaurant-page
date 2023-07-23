ca93024fb2aa10d969eb27c3b9a40251680036a1
baldx <mr.ahmad.m2006@gmail.com>
2023-07-23 21:31:11 +0200

    add event listener for buttons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-const menu = document.querySelector("#menu")
+const menu = document.querySelector(".menu");
+const home = document.querySelector(".home");
+const contact = document.querySelector(".contact");
 
+menu.addEventListener("click", () => {
+    alert("test");
+})

753cac36ab8233fbe17c83fa9cca87a9d824df90
baldx <mr.ahmad.m2006@gmail.com>
2023-07-24 23:43:38 +0200

    import pageInit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-const menu = document.querySelector(".menu");
-const home = document.querySelector(".home");
-const contact = document.querySelector(".contact");
-const content = document.querySelector(".content");
+import pageInit from "./modules/PageInit";
 
-const div = document.createElement("div");
-const img = document.createElement("img");
-
-div.classList.add("header")
-content.append(div)
-
+pageInit()

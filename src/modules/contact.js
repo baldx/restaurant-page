c1a64361db2fb3077945ac3f57cfe89278a0eb72
baldx <mr.ahmad.m2006@gmail.com>
2023-07-25 21:26:14 +0200

    add header & footer

---
+++ src/modules/contact.js
@@ -0,0 +1,47 @@
+function contact() {
+    const content = document.querySelector(".content")
+
+
+    const header = document.createElement("div");
+    header.classList.add("header")
+    content.append(header)
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "/assets/pizza-hut-logo-DBFE2E48AF-seeklogo.com.png")
+    header.appendChild(img);
+
+    const ul = document.createElement("ul");
+    header.appendChild(ul)
+    const liHome = document.createElement("li");
+    const liMenu = document.createElement("li");
+    const liContact = document.createElement("li");
+    ul.appendChild(liHome)
+    ul.appendChild(liMenu)
+    ul.appendChild(liContact)
+
+    const btnHome = document.createElement("button");
+    const btnMenu = document.createElement("button");
+    const btnContact = document.createElement("button");
+
+    liHome.appendChild(btnHome);
+    btnHome.textContent = "Home"
+    btnHome.classList.add("home")
+
+
+    liMenu.appendChild(btnMenu);
+    btnMenu.textContent = "Menu"
+    btnMenu.classList.add("menu")
+
+    liContact.appendChild(btnContact);
+    btnContact.textContent = "Contact"
+    btnContact.classList.add("contact")
+
+
+
+
+    const footer = document.createElement("footer")
+    footer.innerHTML = `Made by <a href="#">baldx</a>`
+    content.appendChild(footer)
+}
+
+export default contact();

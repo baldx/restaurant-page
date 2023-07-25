03bd78d1e5e0b43fdfef27fa8ea8d1137ec1ae2a
baldx <mr.ahmad.m2006@gmail.com>
2023-07-25 21:37:27 +0200

    finish contact

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -36,7 +36,54 @@ function contact() {
     btnContact.textContent = "Contact"
     btnContact.classList.add("contact")
 
+    const main = document.createElement("div")
+    main.classList.add("main");
+    content.appendChild(main);
 
+    const container = document.createElement("div")
+    container.classList.add("container");
+    main.appendChild(container)
+
+    const heading1 = document.createElement("h2")
+    heading1.innerHTML = "Contact"
+    container.appendChild(heading1);
+
+    const card1 = document.createElement("div")
+    const card2 = document.createElement("div")
+    const card3 = document.createElement("div")
+    card1.classList.add("card")
+    card2.classList.add("card")
+    card3.classList.add("card")
+    const heading31 = document.createElement("h3")
+    const heading32 = document.createElement("h3")
+    const heading33 = document.createElement("h3")
+    heading31.innerHTML = "Ronald"
+    heading32.innerHTML = "Tom"
+    heading33.innerHTML = "Arnold"
+    card1.appendChild(heading31)
+    card2.appendChild(heading32)
+    card3.appendChild(heading33)
+    const phone1 = document.createElement("p")
+    const phone2 = document.createElement("p")
+    const phone3 = document.createElement("p")
+    const email1 = document.createElement("p")
+    const email2 = document.createElement("p")
+    const email3 = document.createElement("p")
+    phone1.innerHTML = "Phone: 076 812 73 12"
+    phone2.innerHTML = "Phone: 076 812 73 12"
+    phone3.innerHTML = "Phone: 076 812 73 12"
+    card1.appendChild(phone1);
+    card2.appendChild(phone2);
+    card3.appendChild(phone3);
+    email1.innerHTML = "Email: jldsajd@gmail.com"
+    email2.innerHTML = "Email: jldsajd@gmail.com"
+    email3.innerHTML = "Email: jldsajd@gmail.com"
+    card1.appendChild(email1)
+    card2.appendChild(email2)
+    card3.appendChild(email3)
+    container.appendChild(card1)
+    container.appendChild(card2)
+    container.appendChild(card3)
 
 
     const footer = document.createElement("footer")
